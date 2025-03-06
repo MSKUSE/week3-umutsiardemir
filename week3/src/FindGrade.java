51a7c39aa0dff7c053c66217f29a116a90438ad7
umutsiardemir <umutsiardemir@posta.mu.edu.tr>
2025-03-06 13:16:00 +0300

    week3 added

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -1,6 +1,29 @@
+/*
 public class FindGrade{
 
     public static void main(String[] args){
+        int lenght_of_args=args.length
+        if (lenght_of_args==0) {
+            System.out.println("You should give a score");
+        }
+
+
+
+        int grade = args[0];
+        if(grade>=90) {
+            System.out.println("Grade: A");
+        }
+        else if(grade>=80 && grade<90){
+            System.out.println("Grade: B");
+        }else if(grade>=70 && grade<80){
+            System.out.println("Grade: C");
+        }else if(grade>=60 && grade<70){
+            System.out.println("Grade: D");
+        }else if(grade<60){
+            System.out.println("Grade: E");
+        }else{
+            System.out.println("Enter a valid score");}
+
 
         //Get the score from the user as a file parameter
         //Calculate the grade based on the score
@@ -19,4 +42,4 @@ public class FindGrade{
 
 
 
-}
+}*/
